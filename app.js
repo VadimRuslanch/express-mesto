d90edd6a6aea062b79130c26456263cc33e98e67
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2023-06-12 19:31:15 +0300

    Настройка контроллеров

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const auth = ((req, res, next) => {
+  req.user = {
+    _id: '6485cd144f2bbad724aa4820'
+  };
+  next();
+});
 
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6485cd144f2bbad724aa4820'
-  };
-
-  next();
-});
+app.use(auth);
 app.use(routes);
 
 app.listen(PORT, () => {
--- controllers/users.js
+++ controllers/users.js
@@ -10,9 +10,6 @@ const getUsers = (req, res, next) => {
 };
 
 const getUserById = (req, res, next) => {
-  // req.user = {
-  //   _id: '6485cd144f2bbad724aa4820'
-  // };
   const userId = req.user;
   User.findById(userId)
     .then((users) => {
@@ -56,9 +53,6 @@ const createUser = (req, res, next) => {
 
 const updProfile = (req, res, next) => {
   const { name, about } = req.body;
-  // req.user = {
-  //   _id: '6485cd144f2bbad724aa4820'
-  // };
 
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((user) => {
@@ -79,9 +73,6 @@ const updProfile = (req, res, next) => {
 
 const updAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  // req.user = {
-  //   _id: '6485cd144f2bbad724aa4820'
-  // };
 
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => {
